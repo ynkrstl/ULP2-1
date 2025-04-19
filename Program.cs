bf3d6327c92208dfcfca2b00efd8a5b867331379
ynkrstl <164017924+ynkrstl@users.noreply.github.com>
2025-04-19 12:16:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "input.txt";
+        string outputFile = "output.txt";
+        Random random = new Random();
+
+        using (StreamWriter writer = new StreamWriter(inputFile))
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                writer.WriteLine(random.Next(-100, 101));
+            }
+        }
+
+        int firstOdd = 0;
+        bool found = false;
+
+        using (StreamReader reader = new StreamReader(inputFile))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                int number = int.Parse(line);
+                if (Math.Abs(number) % 2 == 1 && !found)
+                {
+                    firstOdd = number;
+                    found = true;
+                }
+            }
+        }
+
+        using (StreamReader reader = new StreamReader(inputFile))
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                int number = int.Parse(line);
+                writer.WriteLine(number + firstOdd);
+            }
+        }
+
+        Console.WriteLine("Файлы успешно обработаны.");
+        Console.WriteLine($"Первое нечетное число: {firstOdd}");
+    }
+}
